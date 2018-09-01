a73ea5ba761ea2531b97ce92b76c068c769b3655
werasakdangsiw <benza527@gmail.com>
2018-09-01 17:34:14 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const container = require('./container');
+
+
+
+container.resolve(function (users) {
+
+    const app = SetupExpress();
+
+    function SetupExpress() {
+        const app = express();
+        const server = http.createServer(app);
+        server.listen(3000, function () {
+            console.log('Listening on port 3000')
+        });
+        ConfigureExpress(app);
+
+        //Setup router
+        const router = require('express-promise-router')();
+        users.SetRouting(router);
+
+        app.use(router);
+
+        app.use(function (req, res) {
+            res.render('404');
+        });
+    }
+
+    function ConfigureExpress(app) {
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+    }
+
+});
